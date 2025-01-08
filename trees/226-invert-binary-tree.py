c747ee550e5a85085e2f0bbc49f22edab518645a
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-09 02:28:10 +0800

    20250109: 226 invert binary tree - BFS

--- trees/226-invert-binary-tree.py
+++ trees/226-invert-binary-tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 class TreeNode:
@@ -19,4 +20,22 @@ class Solution:
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+        return root
+    
+# BFS
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
         return root
