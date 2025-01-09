27fec570ec1885b9d5aa8b6c62121b47c9b4434c
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-09 16:18:37 +0800

    20250109: 104 max depth of bin tree - BFS

--- trees/104-maximum-depth-of-binary-tree.py
+++ trees/104-maximum-depth-of-binary-tree.py
@@ -1,6 +1,8 @@
 
+from collections import deque
 from typing import Optional
 
+# DFS
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -17,5 +19,19 @@ class Solution:
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
-        
-        
+# BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        lv = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            lv += 1
+        return lv
