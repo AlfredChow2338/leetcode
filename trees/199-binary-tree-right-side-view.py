7aacb1a47e1b453444252e09ce575c3d6f9d7df9
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-16 23:48:57 +0800

    20250116: 199 bst right side view

---
+++ trees/199-binary-tree-right-side-view.py
@@ -0,0 +1,27 @@
+
+from collections import deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque()
+        q.append(root)
+        while q:
+            qLen = len(q)
+            right = None
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    right = node.val
+                    q.append(node.left)
+                    q.append(node.right)
+            if right or right == 0:
+                res.append(right)
+        return res
