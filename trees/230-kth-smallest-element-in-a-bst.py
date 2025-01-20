5cbda04753a4790f3de99d74901fa5abc22bb4eb
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-20 22:40:08 +0800

    20250120: 230 kth smallest element in bst - inorder traversal

--- trees/230-kth-smallest-element-in-a-bst.py
+++ trees/230-kth-smallest-element-in-a-bst.py
@@ -21,3 +21,15 @@ class Solution:
             if n == k:
                 return cur.val
             cur = cur.right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        s = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            s.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return s[k-1]
