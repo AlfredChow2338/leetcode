d2a9d83f4872471ddfe560c20c6b5bf5fd61c815
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-26 18:17:07 +0800

    20250126: 2500 delete greatest value - max heap

--- heap-and-priority-queue/2500-delete-greatest-value-in-each-row.py
+++ heap-and-priority-queue/2500-delete-greatest-value-in-each-row.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 
 # sorting
@@ -6,4 +7,20 @@ class Solution:
         for row in grid:
             row.sort()
         return sum(max(col) for col in zip(*grid))
+            
+# heap
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        count = 0
+        res = 0
+        k = len(grid[0])
+        while count < k:
+            maxV = -1
+            for i in range(len(grid)):
+                grid[i] = [-abs(c) for c in grid[i]]
+                heapq.heapify(grid[i])
+                maxV = max(maxV, -heapq.heappop(grid[i]) )
+            count += 1
+            res += maxV
+        return res
             
