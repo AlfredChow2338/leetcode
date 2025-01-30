6f3d4f5899fe2696f09ea007d14b593fae48d98f
alfredchow2338 <alfredchow2338@gmail.com>
2025-01-30 14:38:44 +0800

    20250130: 208 implement trie prefix tree - trie

--- trie/208-implement-trie-prefix-tree.py
+++ trie/208-implement-trie-prefix-tree.py
@@ -4,6 +4,7 @@
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
 
+# hash map
 class Trie:
     def __init__(self):
         self.words = []
@@ -19,3 +20,37 @@ class Trie:
             if word.startswith(prefix):
                 return True
         return False
+    
+# trie
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
