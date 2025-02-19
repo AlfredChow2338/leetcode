582a14d19dc4c6b9831abee846ffd556d71edc35
alfredchow2338 <alfredchow2338@gmail.com>
2025-02-20 01:18:44 +0800

    20250220: 2730 find the longest semi repetitive substring

--- sliding-window/2730-find-the-longest-semi-repetitive-substring.py
+++ sliding-window/2730-find-the-longest-semi-repetitive-substring.py
@@ -10,3 +10,9 @@ class Solution:
         for j in range(1, len(tab)):
             res = max(res, tab[j] + tab[j-1])
         return max(res, tab[0])
+
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        s = s[0] + s + s[-1]
+        arr = [i for i in range(len(s) - 1) if s[i] == s[i+1]]
+        return max((y - x for x, y in zip(arr, arr[2:])), default = len(s) - 2)
