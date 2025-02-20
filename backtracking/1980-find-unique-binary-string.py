ea503aaa06000873c31db315a9a18c8d378de50a
alfredchow2338 <alfredchow2338@gmail.com>
2025-02-21 00:18:52 +0800

    20250221: 1980 find unique binary string - backtracking

--- backtracking/1980-find-unique-binary-string.py
+++ backtracking/1980-find-unique-binary-string.py
@@ -7,3 +7,17 @@ class Solution:
             curr = nums[i][i]
             res.append("1" if curr == "0" else "0")
         return "".join(res)
+
+# bakctracking
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                if curr not in nums:
+                    return curr
+                return ""
+            add_zero = backtrack(curr + "0")
+            if add_zero:
+                return add_zero
+            return backtrack(curr + "1")
+        return backtrack("")
