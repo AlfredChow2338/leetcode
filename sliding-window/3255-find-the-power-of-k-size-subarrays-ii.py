3d978b29a1eeb8feca0ca47396cf1be039fe391f
alfredchow2338 <alfredchow2338@gmail.com>
2025-02-21 02:03:59 +0800

    20250221: 3255 find the power of k size subarrays II - sliding window

--- sliding-window/3255-find-the-power-of-k-size-subarrays-ii.py
+++ sliding-window/3255-find-the-power-of-k-size-subarrays-ii.py
@@ -25,3 +25,22 @@ class Solution:
                     res.append(maxValue)
             i += 1
         return res
+
+# sliding window
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        count = 1
+        l = 0
+
+        for r in range(len(nums)):
+            if r > 0 and nums[r-1] + 1 == nums[r]:
+                count += 1
+            if r - l + 1 > k:
+                if nums[l] + 1 == nums[l+1]:
+                    count -= 1
+                l += 1
+            if r - l + 1 == k:
+                res.append(nums[r] if count == k else -1)
+
+        return res
