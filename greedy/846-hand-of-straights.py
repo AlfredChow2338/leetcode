98cbd81e43eea600b57f6898e1585b53efcdd25b
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-01 03:51:49 +0800

    20250301: 846 hand of straights - min heap

--- greedy/846-hand-of-straights.py
+++ greedy/846-hand-of-straights.py
@@ -1,5 +1,7 @@
+import heapq
 from typing import Counter, List
 
+# hashmap
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
       if len(hand) % groupSize:
@@ -12,4 +14,32 @@ class Solution:
             if not count[i]:
               return False
             count[i] -= 1
+      return True
+
+# min heap
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+      if len(hand) % groupSize:
+        return False
+
+      count = {}
+      
+      for n in hand:
+        count[n] = 1 + count.get(n, 0)
+      
+      minH = list(count.keys())
+      heapq.heapify(minH)
+
+      while minH:
+        first = minH[0]
+
+        for i in range(first, first + groupSize):
+          if i not in count:
+            return False
+          count[i] -= 1
+          if count[i] == 0:
+            if i != minH[0]:
+              return False
+            heapq.heappop(minH)
+
       return True
