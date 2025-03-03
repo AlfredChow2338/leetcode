adbdee1e0ff12d16c6ba46af4fe17e014c0647ee
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-04 00:39:39 +0800

    20250304: 2161 partition arr according to given pivot - two pointer

--- two-pointers/2161-partition-array-according-to-given-pivot.py
+++ two-pointers/2161-partition-array-according-to-given-pivot.py
@@ -17,3 +17,21 @@ class Solution:
 
         return res
 
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        L, R = 0, len(nums) - 1
+        res = [0] * len(nums)
+
+        for i, j in zip(range(len(nums)), range(len(nums) - 1, -1, -1)):
+            if nums[i] < pivot:
+                res[L] = nums[i]
+                L += 1
+            if nums[j] > pivot:
+                res[R] = nums[j]
+                R -= 1
+
+        while L <= R:
+            res[L] = pivot
+            L += 1
+
+        return res
