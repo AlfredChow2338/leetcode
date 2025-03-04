57320c73f998862970c77fe946054145910cf49a
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-05 00:09:42 +0800

    20250305: 1780 check if num is a sun of powers of 3 - backtracking

--- greedy/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ greedy/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,9 +1,24 @@
 from cmath import log
 
 class Solution:
-    def checkPowersOfThree(self, n: int) -> bool:
-      d = int(log(n, 3))
-      for i in range(d, -1, -1):
-        if n - 3 ** i >= 0:
-          n = n - 3 ** i
-      return n == 0
+  def checkPowersOfThree(self, n: int) -> bool:
+    d = int(log(n, 3))
+    for i in range(d, -1, -1):
+      if n - 3 ** i >= 0:
+        n = n - 3 ** i
+    return n == 0
+
+# backtracking
+class Solution:
+  def checkPowersOfThree(self, n: int) -> bool:
+    def backtrack(i, curr):
+      if curr == n:
+        return True
+      if curr > n or 3 ** i > n:
+        return False
+      if backtrack(i + 1, curr + 3 ** i):
+        return True
+      return backtrack(i+1, curr)
+    return backtrack(0, 0)
+  
+        
