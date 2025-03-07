b294ffa3aa690733d8ee058577b0cc1a14c5fdb2
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-07 16:00:26 +0800

    20250307: 70 climbing stairs - bottom up

--- dynamic-programming/70-climbing-stairs.py
+++ dynamic-programming/70-climbing-stairs.py
@@ -1,3 +1,4 @@
+# top-down
 class Solution:
   def climbStairs(self, n: int) -> int:
     cache = [-1] * n
@@ -10,3 +11,21 @@ class Solution:
       return cache[i]
     
     return backtrack(0)
+
+# bottom-up
+class Solution:
+  def climbStairs(self, n: int) -> int:
+    # 1: 1 (1)
+    # 2: 1+1, 2 (2)
+    # 3: 1+1+1, 1+2, 2+1 (3)
+    # 4: 1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2 (5)
+    # 5: 1+1+1+1+1, 1+1+1+2, 1+1+2+1, 1+2+1+1, 2+1+1+1, 1+2+2, 2+1+2, 2+2+1 (8)
+    if n <= 2:
+      return n
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 2, 3
+    for i in range(3, n + 1):
+      dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+    
