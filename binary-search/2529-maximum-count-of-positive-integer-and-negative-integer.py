86349875cbedc447b5ac39a30b82b8f1fe66e9a6
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-12 23:49:07 +0800

    20250312: 2529 max count of +ve and -ve int - binary search

--- binary-search/2529-maximum-count-of-positive-integer-and-negative-integer.py
+++ binary-search/2529-maximum-count-of-positive-integer-and-negative-integer.py
@@ -13,3 +13,31 @@ class Solution:
         if res >= n - zero - res:
             return res
         return n - zero - res
+    
+# binary search
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        n = len(nums)
+        smallerZero, biggerZero = n, n
+        
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < 0:
+                left = mid + 1
+            else:
+                right = mid - 1 
+                smallerZero = mid
+        
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] <= 0:
+                left = mid + 1
+            else:
+                right = mid - 1
+                biggerZero = mid
+        
+        return max(n - biggerZero, smallerZero)
+
+        
