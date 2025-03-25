d584096bdd6932594e5550717b7c439daff4301a
alfredchow2338 <alfredchow2338@gmail.com>
2025-03-26 00:01:19 +0800

    20250325: fix 3394

--- array/3394-check-if-grid-can-be-cut-into-sections.py
+++ array/3394-check-if-grid-can-be-cut-into-sections.py
@@ -1,10 +1,12 @@
+from typing import List
+
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         # step 1: scan left to right
         # step 2: scan top to bottom
         # step 3: check whether counts from step 1 and 2 > 3
         x = [(r[0], r[2]) for r in rectangles]
-        y = [([1], r[3]) for r in rectangles]
+        y = [(r[1], r[3]) for r in rectangles]
         x.sort()
         y.sort()
         def count(intervals):
