1801f12e43d62176b2620657d8d440977c697c37
alfredchow2338 <alfredchow2338@gmail.com>
2025-04-06 20:46:08 +0800

    20250406: 368 largest divisible subset: bottom up

--- dynamic-programming/368-largest-divisible-subset.py
+++ dynamic-programming/368-largest-divisible-subset.py
@@ -38,3 +38,19 @@ class Solution:
             memo[(i, prev)] = res
             return res
         return backtrack(0, 1)
+    
+# bottom-up
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums.sort()
+        res = []
+        dp = [[num] for num in nums]
+        for i in reversed(range(n)):
+            for j in range(i + 1, n):
+                if nums[j] % nums[i] == 0: 
+                    tmp = [nums[i]] + dp[j]
+                    dp[i] = tmp if len(tmp) > len(dp[i]) else dp[i]
+            res = dp[i] if len(dp[i]) > len(res) else res
+        return res
+
